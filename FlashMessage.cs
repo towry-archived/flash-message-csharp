2b7bf427939cf2ad1cb491fa12375df1569faa7b
towry <tovvry@gmail.com>
2015-04-24 07:23:21 +0800

    init

---
+++ FlashMessage.cs
@@ -0,0 +1,66 @@
+﻿//
+// Copyright (c) 2015 Towry Wang
+// All rights reserved
+//
+//    http://github.com/towry/flash-message-csharp
+// 
+// Under MIT License
+//
+
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// FlashMessage
+/// Display the message once
+/// </summary>
+public static class FlashMessage
+{
+    public static bool Has(this System.Web.UI.Page page)
+    {
+        if (page.Response.Cookies["flash_message"] != null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static void Flash(this System.Web.UI.Page page)
+    {
+        if (page.Response.Cookies["flash_message"] != null)
+        {
+            page.Response.Write(page.Response.Cookies["flash_message"].Value);
+            page.Response.Cookies.Remove("flash_message");
+        }
+    }
+
+    public static void Flash(this System.Web.UI.Page page, string value)
+    {
+        page.Response.Cookies["flash_message"].Value = value;
+        page.Response.Cookies["flash_message"].Expires = DateTime.Now.AddDays(7);
+    }
+}
---
+++ readme.markdown
@@ -0,0 +1,32 @@
+# FlashMessage
+
+*Intro*: FlashMessage is a helper class used in asp.net webform 
+to display message once.
+
+I was helping my classmate to improve her asp.net website, but I am
+not very familiar with asp.net stuff, so the code might be not in the
+correct way.
+
+## Usage
+
+Put this file in your `App_Code` folder, in your `cs` file, set the message:
+
+```cs
+FlashMessage.Flash(this, "Wrong password, please try again");
+```
+In your 'aspx' file, to display the message that you set before:
+
+```cs
+<%
+if (FlashMessage.Has(this)) {
+    FlashMessage.Flash(this);
+}
+%>
+```
+
+Use `FlashMessage.Has` method to test if there has a message, remeber always pass
+this (System.Web.UI.Page instance) to the message.
+
+---
+
+Copyright (c) 2015, Towry Wang
